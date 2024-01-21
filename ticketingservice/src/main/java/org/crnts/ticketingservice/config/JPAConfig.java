1a885e64611df02df8f7a08bc528b3797ba0d905
Madhura Choukanavar <Choukanavarmadhura6@gmail.com>
2024-01-21 11:42:08 +0530

    bean entity for ticketing

---
+++ ticketingservice/src/main/java/org/crnts/ticketingservice/config/JPAConfig.java
@@ -0,0 +1,13 @@
+package org.crnts.ticketingservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration 
+@EnableJpaRepositories(basePackages = { "org.crnts.ticketingservice" })
+@EnableTransactionManagement
+public class JPAConfig {
+
+}
+
