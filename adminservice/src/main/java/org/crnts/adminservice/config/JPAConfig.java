885eb8548d8b9f1a61b8dc2728b0cf5c37ce2e3d
KalyaniNagula <kalyanreddynagula@gmail.com>
2024-01-21 13:29:15 +0530

    first commit

---
+++ adminservice/src/main/java/org/crnts/adminservice/config/JPAConfig.java
@@ -0,0 +1,12 @@
+package org.crnts.adminservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableJpaRepositories(basePackages = { "org.crnts.adminservice" })
+@EnableTransactionManagement
+public class JPAConfig {
+	
+}
---
+++ adminservice/src/main/java/org/crnts/adminservice/serviceimpl/DepartmentServiceImp.java
@@ -0,0 +1,71 @@
+package org.crnts.adminservice.serviceimpl;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.crnts.adminservice.controller.DepartmentController;
+import org.crnts.adminservice.entity.DepartmentEntity;
+import org.crnts.adminservice.repository.DepartmentRepository;
+import org.crnts.adminservice.service.DepartmentService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+import lombok.extern.slf4j.Slf4j;
+@Slf4j
+@Service
+public class DepartmentServiceImp implements DepartmentService {
+	private static Logger log=LoggerFactory.getLogger(DepartmentController.class);
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	@Override
+	public void save(DepartmentEntity departmentEntity) {
+		departmentRepository.save(departmentEntity);
+		log.info("Department details saved",departmentEntity);
+	}
+
+	@Override
+	public DepartmentEntity get(Long departmentId) {
+		
+		return departmentRepository.findById(departmentId).get();
+	}
+
+	@Override
+	public List<DepartmentEntity> getAll() {
+		
+		return departmentRepository.findAll();
+	}
+
+	@Override
+	public void update(DepartmentEntity departmentEntity) {
+	Optional<DepartmentEntity>optional	=departmentRepository.findById(departmentEntity.getDepartmentId());
+		if(optional.isPresent()) {
+			departmentRepository.save(departmentEntity);
+			System.out.println("update successfull");
+		}
+		else {
+			optional.orElseThrow();
+		}
+		
+	}
+
+	@Override
+	public DepartmentEntity delete(Long departmentId) {
+		Optional<DepartmentEntity>optional	=departmentRepository.findById(departmentId);
+		if(optional.isPresent()) {
+			departmentRepository.deleteById(departmentId);;
+			System.out.println("deleted successfull");
+		}
+		else {
+			optional.orElseThrow();
+		}
+		return null;
+		
+		
+	}
+
+}
