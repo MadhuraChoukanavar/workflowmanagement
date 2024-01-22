7fc8dee01424b3ef94c4a090b94397286d1d5a08
chetanapinki <naikchetana6@gmail.com>
2024-01-22 12:41:52 +0530

    :

---
+++ adminservice/src/main/java/org/crnts/adminservice/service/DepartmentIssueService.java
@@ -0,0 +1,13 @@
+package org.crnts.adminservice.service;
+
+import java.util.List;
+
+
+
+import org.crnts.adminservice.entity.DepartmentIssueEntity;
+
+public interface DepartmentIssueService {
+void saveIssueType(DepartmentIssueEntity issueEntity);
+List<DepartmentIssueEntity> getAll();
+void deleteAll();
+}
