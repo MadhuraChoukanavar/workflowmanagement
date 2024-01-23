0ba7b9c6cf05936db16641c9c4179e00efb8f6cd
Anisha-00 <ranianisha456@gmail.com>
2024-01-23 14:32:22 +0530

    taking

--- adminservice/src/main/java/org/crnts/adminservice/config/JPAConfig.java
+++ adminservice/src/main/java/org/crnts/adminservice/config/JPAConfig.java
@@ -5,15 +5,9 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-<<<<<<< HEAD
-@EnableJpaRepositories(basePackages = { "org.crnts.adminservice.bean","org.crnts.adminservice.repository" })
-@EnableTransactionManagement
-public class JPAConfig {
-	
-=======
 @EnableJpaRepositories(basePackages = { "org.crnts.adminservice" })
 @EnableTransactionManagement
 public class JPAConfig {
->>>>>>> master
+
 
 }
