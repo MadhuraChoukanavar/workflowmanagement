873f8a44d7a03ed4a74858316dce38670c0d9b7e
Madhura Choukanavar <Choukanavarmadhura6@gmail.com>
2024-01-23 10:18:55 +0530

    change

--- ticketingservice/src/main/java/org/crnts/ticketingservice/config/TicketingConfig.java
+++ ticketingservice/src/main/java/org/crnts/ticketingservice/config/TicketingConfig.java
@@ -1,7 +1,9 @@
 package org.crnts.ticketingservice.config;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -19,6 +21,10 @@ public class TicketingConfig {
 			}
 		};
 	}
-	
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+
 
 }
