cb155584229750b12f23255d961449ec60244d6a
Madhura Choukanavar <Choukanavarmadhura6@gmail.com>
2024-01-30 15:08:21 +0530

    final

--- ticketingservice/src/main/java/org/crnts/ticketingservice/config/ticketingconfig.java
+++ ticketingservice/src/main/java/org/crnts/ticketingservice/config/ticketingconfig.java
@@ -1,6 +1,5 @@
 package org.crnts.ticketingservice.config;
 
-import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 
 import org.springframework.context.annotation.Configuration;
@@ -8,27 +7,24 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class TicketingConfig {
-
+	
 	@Bean
 	WebMvcConfigurer mvcConfigurer() {
 		return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedHeaders("*").allowedMethods("*").allowedOrigins("*");
+				registry.addMapping("/**").allowedHeaders("*").allowedMethods("*")
+						.allowedOrigins("*");
 			}
 		};
 	}
-
 	@Bean
 	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
-	@Bean
-	public ModelMapper modelMapper()
 
-	{
-		return new ModelMapper();
-	}
+
 }
